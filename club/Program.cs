eb1feb656210d8b3c78303363edf3ee95b4fb024
AgustinBowen <agus_bowen@hotmail.com>
2023-06-17 17:51:12 -0300

    no anduvieron las ramas jajaj queda asi nomas

--- club/Program.cs
+++ club/Program.cs
@@ -18,7 +18,6 @@ namespace clubApp
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run( MainView.Instance); // uso de la unica instancia admitida en el sistema.
 
-            //ESTO ES UNA PRUEBA DE LA RAMA DE LA PC
         }
     }
 }
