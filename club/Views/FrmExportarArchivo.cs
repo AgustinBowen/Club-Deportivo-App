fa28d0245433f69bf0d078df3840658bbed02166
quemerotw <quemerotw4400@gmail.com>
2023-06-24 10:53:07 -0300

    Exportar a txt generico

--- club/Views/FrmExportarArchivo.cs
+++ club/Views/FrmExportarArchivo.cs
@@ -13,7 +13,7 @@ namespace clubApp.Views
 {
     public partial class FrmExportarArchivo : Form
     {
-        List<Socio> _lista;
+        List<object> _lista;
         public FrmExportarArchivo()
         {
             InitializeComponent();
@@ -21,7 +21,37 @@ namespace clubApp.Views
         }
 
         public void ShowExportar(List<Socio> lista){
-            this._lista = lista;
+            this._lista = lista.ToList<object>();
+            this.Show();
+        }
+        public void ShowExportar(List<Profesor> lista)
+        {
+            this._lista = lista.ToList<object>();
+            this.Show();
+        }
+        public void ShowExportar(List<Localidad> lista)
+        {
+            this._lista = lista.ToList<object>();
+            this.Show();
+        }
+        public void ShowExportar(List<Actividad> lista)
+        {
+            this._lista = lista.ToList<object>();
+            this.Show();
+        }
+        public void ShowExportar(List<Cuota> lista)
+        {
+            this._lista = lista.ToList<object>();
+            this.Show();
+        }
+        public void ShowExportar(List<Horario> lista)
+        {
+            this._lista = lista.ToList<object>();
+            this.Show();
+        }
+        public void ShowExportar(List<TipoActividad> lista)
+        {
+            this._lista = lista.ToList<object>();
             this.Show();
         }
 
